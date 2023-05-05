e65341c4011e6e4f5b66284bbf93a525b2ff9f95
Ben Roberts <bendando@hotmail.co.uk>
2023-05-05 17:31:36 +0100

    eslint fix

--- src/pages/blogs/blog-grid-sidebar/page/[page].tsx
+++ src/pages/blogs/blog-grid-sidebar/page/[page].tsx
@@ -86,7 +86,7 @@ export const getStaticProps: GetStaticProps<TProps, Params> = ({ params }) => {
     );
 
     const { blogs: recentPosts } = getAllBlogs(["title"], 0, 5);
-    const tags = null; //getTags();
+    const tags = null; // getTags();
     return {
         props: {
             data: {
