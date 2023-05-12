5f6fc9734f5caeefe3a18de857010f8de9f9bfd0
Ben Roberts <bendando@hotmail.co.uk>
2023-05-12 10:45:19 +0100

    redirecting to home

--- src/pages/api/draft/[...route].ts
+++ src/pages/api/draft/[...route].ts
@@ -1,20 +1,21 @@
 /* eslint-disable */
 
 // route handler enabling draft mode
-import { draftMode } from 'next/headers';
+// import { draftMode } from 'next/headers';
  
 //async 
-export default function  (request: any) {//GET(request: Request) {
+export default function  (request: any, response: any) {//GET(request: Request) {
 
     const query = request.query;
     console.log(JSON.stringify(query));
+    console.log(request.body);
     console.log("we have a request");
 
   // console.log(request);
-  draftMode().enable();
-  return new Response('Draft mode is enabled');
-
-  
+  response.setDraftMode({ enable: true });
+  //return new Response('Draft mode is enabled');
+  //response.status(200).json({ name: 'Not implemented at this time' });
+  response.redirect("/");
 }
 
 /* eslint-enable */
